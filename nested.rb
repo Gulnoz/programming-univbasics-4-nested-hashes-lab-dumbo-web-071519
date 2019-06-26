7ce6926b920fc747540d987634992927aaea3591
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-06-26 01:36:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,15 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-key = :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
     programmer_hash[:yukihiro_matsumoto]={
       :known_for => "Ruby",
       :languages => ["LISP", "C"]
    }
-
+return programmer_hash
 end
 
 def changing_alan
